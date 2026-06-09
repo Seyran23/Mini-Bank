58f4d1c03d4ad5a25623213adf43d4f6f02d80fd
Seyran Musayev <seyranm230@gmail.com>
2026-06-09 15:39:55 +0200

    chore: initialize monorepo with Turborepo, pnpm workspaces, and tooling

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+  env: {
+    node: true,
+  },
+  ignorePatterns: ['dist/', 'node_modules/', '*.js', '!.eslintrc.js'],
+};
